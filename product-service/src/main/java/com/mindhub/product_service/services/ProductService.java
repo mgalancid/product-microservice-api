31eba564482be84d3ad9e5c4c0cad93bd49904e9
Max <53160586+mgalancid@users.noreply.github.com>
2025-01-23 19:48:26 -0300

    Added getProductsById

--- product-service/src/main/java/com/mindhub/product_service/services/ProductService.java
+++ product-service/src/main/java/com/mindhub/product_service/services/ProductService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface ProductService {
     List<ProductEntityDTO> getAllProducts();
+    List<ProductEntityDTO> getProductsById(List<Long> ids);
     ProductEntityDTO createProduct(NewProductEntityDTO newProduct);
     ProductEntityDTO updateProductStock(Long productId, Integer newStock);
 }

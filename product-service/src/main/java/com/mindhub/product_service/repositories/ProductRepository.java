2bc4d49ec874d8c158ff59f3f0f1bf9cad79ee98
Max <53160586+mgalancid@users.noreply.github.com>
2025-01-23 10:58:14 -0300

    Added derived methods

--- product-service/src/main/java/com/mindhub/product_service/repositories/ProductRepository.java
+++ product-service/src/main/java/com/mindhub/product_service/repositories/ProductRepository.java
@@ -3,5 +3,10 @@ package com.mindhub.product_service.repositories;
 import com.mindhub.product_service.models.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository <ProductEntity, Long> {
+    ProductEntity findByName(String name);
+    boolean existsByProduct();
+    boolean existsByStock();
 }
